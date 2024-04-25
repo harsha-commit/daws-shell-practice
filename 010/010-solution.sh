bcb49459a2245e56daa6c272398285fd602b541d
Harsha Vardhan Bashavathini <harshavardhan.bashavathini@gmail.com>
2024-04-25 18:00:10 +0530

    profile

--- 010/010-solution.sh
+++ 010/010-solution.sh
@@ -2,9 +2,18 @@
 
 # RUN THIS IN A RHEL-9 INSTANCE
 
+# Install package, store logs, use functions
+
 USER_ID=$(id -u)
-TIMESTAMP=$(date )
-LOGFILE="/tmp/TIMESTAMP-SCRIPTNAME.log"
+TIMESTAMP=$(date +"%F-%H-%M-%S")
+SCRIPTNAME=$(echo $0 | cut -d '.' -f 1)
+LOGFILE="/tmp/$TIMESTAMP-$SCRIPTNAME.log"
+R="\e[31m"
+G="\e[32m"
+Y="\e[33m"
+W="\e[0m"
+
+PACKAGES=[ "git" "mysql" "docker" ]
 
 CHECK_ROOT(){
     if [ $USER_ID -ne 0 ]
@@ -20,14 +29,17 @@ CHECK_ROOT(){
 VALIDATE(){
     if [ $1 -ne 0 ]
     then
-        echo "$2... FAILED"
+        echo "$2...$R FAILED$W"
         exit 1
     else
-        echo "$2... SUCCESS"
+        echo "$2...$G SUCCESS$W"
 }
 
-dnf install mysql -y
-VALIDATE $? "Installing MySQL"
+# Looping arguments
+for 
 
-dnf install git -y
+dnf install mysql -y &>> $LOGFILE
+VALIDATE $? "Installing MySQL"
+ 
+dnf install git -y &>> $LOGFILE
 VALIDATE $? "Installing Git"
