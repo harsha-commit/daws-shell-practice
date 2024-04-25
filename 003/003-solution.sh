a4065db6c7192d44fc4f8d7752016aa260c8c79b
Harsha Vardhan Bashavathini <harshavardhan.bashavathini@gmail.com>
2024-04-25 17:06:02 +0530

    profile

--- 003/003-solution.sh
+++ 003/003-solution.sh
@@ -1,7 +1,18 @@
 #!/bin/bash
 
-echo "hello"
+# Download data from URL
+curl -O "https://raw.githubusercontent.com/harsha-commit/daws-shell-practice/main/utilities/data.csv?token=GHSAT0AAAAAACOKJAZFF4BK32PCHS7SAMUYZRKCX3A"
 
-echo "Opening an Editor: "
-vim hello.txt
-echo "Editor closed"
+# Rename the downloaded file to users.csv
+mv data.csv users.csv
+
+# Show last five users from users.csv
+tail -n 5 users.csv
+
+# Create a test and prod directory
+mkdir test
+
+# Copy users.csv to test, but rename as test-users.csv
+cp ./users.csv ./test/test-users.csv
+
+# Show the contents of, in read-only mode
--- 003/task.md
+++ 003/task.md
@@ -1,3 +1,5 @@
 # Assignment (Week 1 - Day 3)
 
 Write a shell script (or use command line) to perform the following
+
+1. Download data.csv from https://raw.githubusercontent.com/harsha-commit/daws-shell-practice/main/utilities/data.csv?token=GHSAT0AAAAAACOKJAZFF4BK32PCHS7SAMUYZRKCX3A using curl
