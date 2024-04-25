fe4592c0cfa7c71771c1bf403950e166d7922f94
Harsha Vardhan Bashavathini <harshavardhan.bashavathini@gmail.com>
2024-04-25 19:55:36 +0530

    profile

--- 013/013-solution.sh
+++ 013/013-solution.sh
@@ -8,18 +8,5 @@ chmod 700 ./utility.sh
 
 source ./utility.sh
 
-# CHECK_ROOT
-# echo "$@"
-# INSTALL_PACKAGES $@
-
-for i in $@
-    do
-        dnf list installed $i &>> $LOGFILE
-        if [ $? -ne 0 ]
-        then
-            dnf install $i -y &>> $LOGFILE
-            VALIDATE $? "Installing $i"
-        else
-            echo -e "$i already installed...$Y SKIPPING$W"
-        fi
-    done
+CHECK_ROOT
+INSTALL_PACKAGES $@
